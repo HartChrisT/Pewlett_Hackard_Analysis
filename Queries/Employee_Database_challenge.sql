959799ea3df4364b0a9031cd25ad00aef8a308ac
HartChrisT <92996865+HartChrisT@users.noreply.github.com>
2022-02-05 20:08:19 -0500

    adding additional queries

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -47,3 +47,35 @@ FROM employees as e
 WHERE (de.to_date = '9999-01-01') 
 	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
+
+--Aditional queries
+SELECT COUNT(title),
+			title
+INTO ment_elig_tit_count
+FROM mentorship_eligibility
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   ti.title
+INTO ment_ent   
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as ti
+		ON (de.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01') 
+	AND (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT COUNT(title),
+			title
+INTO ment_ent_count
+FROM ment_ent
+GROUP BY title
+ORDER BY COUNT(title) DESC;
